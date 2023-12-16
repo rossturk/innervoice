7130db53054f65dd1ef62d7c4426092387444514
Ross Turk <ross@floxdev.com>
2023-12-15 20:23:19 -0500

    new flox version!
    Signed-off-by: Ross Turk <ross@floxdev.com>

--- build.sh
+++ build.sh
@@ -8,6 +8,13 @@ mkdir -p build
 cp -f local_options ./driver/src/
 
 cd build
-cmake ../driver -DPACKAGE_UIDS=OFF -DUSE_JEMALLOC=OFF -DPACKAGE_DB=OFF
+cmake ../driver \
+	-DPACKAGE_UIDS=OFF \
+	-DCMAKE_POLICY_DEFAULT_CMP0074=NEW \
+	-DUSE_JEMALLOC=OFF \
+	-DMYSQL_INCLUDE_DIR=$FLOX_ENV/include/mysql \
+	-DLIBEVENT_INCLUDE_DIRS=$FLOX_ENV/include \
+	-DLIBEVENT_LIBRARIES=$FLOX_ENV/lib
+
 make -j8 install
 cd ..
